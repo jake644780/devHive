fb815508162984afc953889525c0ebf6c7d38e52
jake644780 <yesn7722@gmail.com>
2025-03-11 18:48:24 +0100

    added package

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,19 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const console_1 = require("console");
-(0, console_1.log)("helo mom");
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const routes_1 = __importDefault(require("./routes/routes"));
+//configs
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 8081;
+//middlewares
+app.use(express_1.default.json());
+(0, routes_1.default)(app);
+app.listen(PORT, () => {
+    (0, console_1.log)("app listening on port " + PORT);
+});
--- backend/app.ts
+++ backend/app.ts
@@ -1,3 +1,16 @@
-import { log } from "console";
+import { log } from 'console';
+import express, { Request, Response, Express } from 'express';
+import dotenv from 'dotenv';
+import setupRoutes from "./routes/routes";
+//configs
+dotenv.config();
+const app: Express = express();
+const PORT = process.env.PORT || 8081;
+//middlewares
+app.use(express.json());
 
-log("helo mom");
+setupRoutes(app);
+
+app.listen(PORT, ()=>{
+    log("app listening on port " + PORT);
+});
