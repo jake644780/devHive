62965033d5fe509c69192432430e6d19f71637af
jake644780 <yesn7722@gmail.com>
2025-03-11 20:02:13 +0100

    add backend port configuration and update app initialization

--- backend/app.ts
+++ backend/app.ts
@@ -2,12 +2,14 @@ import { log } from 'console';
 import express, { Request, Response, Express } from 'express';
 import dotenv from 'dotenv';
 import setupRoutes from "./routes/routes";
+
 //configs
 dotenv.config();
 const app: Express = express();
 const PORT = process.env.PORT || 8081;
 //middlewares
 app.use(express.json());
+
 
 setupRoutes(app);
 
