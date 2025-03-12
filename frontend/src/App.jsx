e16dc3315c60fc37a935db483d721946403a05d7
jake644780 <yesn7722@gmail.com>
2025-03-12 09:37:11 +0100

    add react-router-dom and implement routing in App component; enhance Navbar with dynamic navigation items

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg';
 import {Navbar} from './navbar/index';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 
 function App() {
   return (
-    <>
-      <Navbar/>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <div className="container">
+        
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- frontend/src/navbar/navbar.jsx
+++ frontend/src/navbar/navbar.jsx
@@ -1,15 +1,27 @@
 import styles from "./style.module.css";
 
 export default function Navbar(){
+
+    let navItems = [];
+    let navNames = [
+        'home',
+        'add idea',
+    ];
+    for (let i = 0; i < navNames.length; i++) navItems.push(
+        <li className={styles.navElement}>
+            { navNames[i] }
+        </li>
+    );
+        
+
+
     return (
         <nav className={styles.navbar}>
             <div className={styles.home}>
                 Home
             </div>
-            <ul>
-                <li>idk1</li>
-                <li>idk2</li>
-                <li>idk3</li>
+            <ul className={styles.navElements}>
+                { navItems }
             </ul>
         </nav>
     );
