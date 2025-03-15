488188675bbb5fff6079524752cda805a0d9de9c
jake644780 <yesn7722@gmail.com>
2025-03-15 13:53:22 +0100

    refactor routing structure in App component; replace custom Routes component with direct Routes usage and simplify Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg';
-import {Navbar} from './assets/navbar/index';
-import { BrowserRouter as Router} from "react-router-dom";
-import {Routess} from "./routes/index";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {Navbar} from './assets/navbar';
+import {Home} from './assets/home';
+import {AddIdea} from './assets/addIdea';
 
 function App() {
   return (
     <Router>
-      <Routess />
       <Navbar />
-      <div className="container">
-        
-      </div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/AddIdea" element={<AddIdea/>} />
+      </Routes>
     </Router>
   );
 }
--- frontend/src/assets/navbar/navbar.jsx
+++ frontend/src/assets/navbar/navbar.jsx
@@ -1,33 +1,17 @@
 import styles from "./style.module.css";
 import { Link } from "react-router-dom";
 
-
-
 export default function Navbar(){
-
-    let navItems = [];
-    let navNames = [
-        'home',
-        'addIdea',
-    ];
-    for (let i = 0; i < navNames.length; i++) navItems.push(
-        <li className={styles.navElement}>
-            <Link to={"/" + navNames[i]}>
-                { navNames[i] }
-            </Link>
-        </li>
-    );
-        
-
-
-    return (
-        <nav className={styles.navbar}>
-            <div className={styles.home}>
-                Home
-            </div>
-            <ul className={styles.navElements}>
-                { navItems }
+    return(
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/addIdea">addIdea</Link>
+                </li>
             </ul>
-        </nav>
+        </div>
     );
 }
