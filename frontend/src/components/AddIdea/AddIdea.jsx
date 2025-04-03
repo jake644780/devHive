640d10b77667dd44f1544bc8336dd9a85839e2b4
jake644780 <yesn7722@gmail.com>
2025-04-03 21:36:44 +0200

    feat: enhance AddIdea and Ideas components with form handling, data fetching, and delete functionality

--- frontend/src/components/AddIdea/AddIdea.jsx
+++ frontend/src/components/AddIdea/AddIdea.jsx
@@ -1,15 +1,54 @@
+import { useState } from "react";
 import styles from "./style.module.scss";
 
 export default function AddIdea(){
+    let [category, setCategory] = useState("");
+    let [content, setContent] = useState("");
+    let [error, setError] = useState("");
+
+
+    const handleCategoryChange = (event) => {
+        setCategory(event.target.value); // Update category state
+    };
+
+    const handleContentChange = (event) => {
+        setContent(event.target.value); // Update content state
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log({ category, content });
+        let data = {category, content};
+        const fetchData = async () => {
+            try {
+              const response = await fetch('http://localhost:8080/idea/new',{
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+              });
+              const result = await response.json();
+              setCategory("");
+              setContent("");
+              alert("idea added");
+            } catch (error) {
+              setError(error);
+            }
+          };
+          fetchData();
+    };
+
+    
     return (
     <div className={styles.content}>
         <form className={styles.container}>
             <div className={styles.title}>submit a new idea</div>
             <label htmlFor="category">category</label>
-            <input type="text" id="category" placeholder="microservices, ml, etc." />
+            <input type="text" id="category" placeholder="microservices, ml, etc." onChange={handleCategoryChange} value={category} />
             <label htmlFor="content">content</label>
-            <textarea id="content"></textarea>
-            <button type="submit">submit</button>
+            <textarea id="content" onChange={handleContentChange} value={content}></textarea>
+            <button type="submit" onClick={handleSubmit}>submit</button>
         </form>
     </div>
     )
--- frontend/src/components/Ideas/Ideas.jsx
+++ frontend/src/components/Ideas/Ideas.jsx
@@ -2,29 +2,46 @@ import React, { useState, useEffect } from 'react';
 import styles from "./style.module.scss";
 
 export default function Ideas (){
-    const [data, setData] = useState(null);
+    const [ideas, setIdeas] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
   
+
+    const deleteIdeaFromBackend = async (index) => {
+      try {
+        const response = await fetch(`http://localhost:8080/idea/${index}`, {method: "DELETE"});
+        const result = await response.json();
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    const deleteIdea = (index) => {
+      const updatedIdeas = ideas.filter((idea) => idea.id !== index);
+      setIdeas(updatedIdeas);
+      deleteIdeaFromBackend(index);
+    };
+
+    const fetchIdeas = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch('http://localhost:8080/ideas');
+        const result = await response.json();
+        
+        setIdeas(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     useEffect(() => {
-        // Function to fetch data
-        const fetchData = async () => {
-          try {
-            setLoading(true);
-            const response = await fetch('http://localhost:8080/ideas');
-            const result = await response.json();
-            
-            setData(result);
-          } catch (error) {
-            setError(error);
-          } finally {
-            setLoading(false);
-          }
-        };
-    
-        fetchData();
+        fetchIdeas();
       }, []);
     
+
+      
 
       if (loading) return <p>Loading...</p>;
       if (error) return <p>Error: {error.message}</p>;
@@ -35,11 +52,12 @@ export default function Ideas (){
         <div>
           <h1>Fetched Data</h1>
             {
-            data.map((idea, i) => (
-              <div key={i}>
-                <h3>Idea {i + 1}:</h3>
-                <p>{idea.description}</p>
+            ideas.map((idea, i) => (
+              <div key={idea.id}>
+                <h3>Idea {idea.id}:</h3>
                 <p><strong>Category:</strong> {idea.category}</p>
+                <p><strong>Content:</strong> {idea.content}</p>
+                <button onClick={()=>deleteIdea(idea.id)}>delete idea</button>
               </div>
             ))
             }
@@ -47,4 +65,4 @@ export default function Ideas (){
       );
 
 
-};
+};3
